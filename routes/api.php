b3814b804b59de0d9e9e1b77d0e7b856463f827f
busydog1G <stu112114@gmail.com>
2021-01-07 17:28:16 +0800

    student route api

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\StudentController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::apiResource('students',StudentController::class);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
